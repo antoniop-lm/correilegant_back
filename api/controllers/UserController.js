9384f0385266f10c52cce2c12e16e4aa7201a4a9
Antônio Pedro <Antnio Pedro>
2016-05-20 15:24:20 -0300

    Follow e Unfollow funcionando

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -35,6 +35,64 @@ module.exports = {
     }
     else
       return res.json(context);
+  },
+
+  follow: function (req, res) {
+    /*
+      user
+      following
+    */
+    var context = {};
+    context.status = 'error';
+
+    console.log(req.body);
+
+    var data = (req.body.formdata) ? req.body.formdata : undefined;
+    if (data) {
+      try {
+        User.findOne({where: {username: data.following}}).exec(function(err, result){
+          if(err) throw err;
+          data.following = result.id;
+
+          User.findOne({where: {username: data.user}}).exec(function(err, result){
+            if(err) throw err;
+            result.follow.add(data.following);
+            result.save(function(err) {if(err) throw err;});
+            context.status = 'success';
+            return res.json(context);
+          });
+        });
+      } catch (err) {return res.json(context);}
+    } else return res.json(context);
+  },
+
+  unfollow: function (req, res) {
+    /*
+      user
+      following
+    */
+    var context = {};
+    context.status = 'error';
+
+    console.log(req.body);
+
+    var data = (req.body.formdata) ? req.body.formdata : undefined;
+    if (data) {
+      try {
+        User.findOne({where: {username: data.following}}).exec(function(err, result){
+          if(err) throw err;
+          data.following = result.id;
+
+          User.findOne({where: {username: data.user}}).exec(function(err, result){
+            if(err) throw err;
+            result.follow.remove(data.following);
+            result.save(function(err) {if(err) throw err;});
+            context.status = 'success';
+            return res.json(context);
+          });
+        });
+      } catch (err) {return res.json(context);}
+    } else return res.json(context);
   }
 };
 

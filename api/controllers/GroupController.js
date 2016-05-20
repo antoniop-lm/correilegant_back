b5ffcf1a2ba7c3f83e3e52141bd41b531f64ce1d
Antônio Pedro <Antnio Pedro>
2016-05-20 14:22:30 -0300

    Todos os metodos new funcionando

--- api/controllers/GroupController.js
+++ api/controllers/GroupController.js
@@ -6,6 +6,32 @@
  */
 
 module.exports = {
-	
+  new: function (req, res) {
+    /*
+      owner
+      name
+      users
+    */
+    var context = {};
+    context.status = 'error';
+
+    console.log(req.body);
+
+    var data = (req.body.formdata) ? req.body.formdata : undefined;
+    if (data) {
+      try {
+        User.findOne({where: {username: data.owner}}).exec(function(err, result){
+          if(err) throw err;
+          data.owner = result.id;
+
+          Group.create(data).exec(function(err, created){
+            if(err) throw err;
+            context.status = 'success';
+            return res.json(context);
+          });
+        });
+      } catch (err) {return res.json(context);}
+    } else return res.json(context);
+  }	
 };
 

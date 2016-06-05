8c61693770f7c1dcb6d22e31aaed5291d9efc4b0
Ana Paula dos Reis Lima <anapauladosreislima@gmail.com>
2016-06-05 17:47:22 -0300

    Tudo de grupos funciona

--- api/controllers/GroupController.js
+++ api/controllers/GroupController.js
@@ -6,31 +6,65 @@
  */
 
  module.exports = {
-	new_group: function (req, res) {
+	set_group: function (req, res) {
 	    /*
 	      name
+	      *id
+		  id_users
 	    */
 	    var context = {};
 	    context.status = 'error';
 
 	    console.log(req.body);
 
-	    var data = (req.body.formdata) ? req.body.formdata : undefined;
+	    var data = (req.body) ? req.body : undefined;
 	    if (data) {
 	      	try {
 	      		data.owner = req.user.id;
-      			Group.create(data).exec(function(err, created){
-      				if(err) throw err;
-      				created.members.add(data.owner);
-  					created.save(function(err) {
-  						if(err) throw err;
-  						context.status = 'success';
-  						return res.json(context);
-  					});
-      			});
+	      		var group = null;
+	      		if(data.id){ 
+	      			Group.update({"id": data.id, "owner": req.user.id}, data).exec(function(err, updated){
+	      				if (err || !updated) throw err;
+	      				// updated.members.add(data.id_users);
+	  					// updated.save(function(err) {
+	  						if(err) throw err;
+	  						context.status = 'success';
+	  						return res.json(context);
+	  					// });
+	      			});
+	      		} else {
+	      			Group.create(data).exec(function(err, created){
+	      				if(err) throw err;
+	      				created.members.add(data.id_users);
+	  					created.save(function(err) {
+	  						if(err) throw err;
+	  						context.status = 'success';
+	  						return res.json(context);
+	  					});
+	      			});
+	      		}
+      			
 
 	      	} catch (err) {return res.json(context);}
 	    } else return res.json(context);
+  	},
+
+  	my_groups: function(req, res) {
+  		var context = {};
+	    context.status = 'error';
+	    try {
+	    	User.findOne(req.user.id).populate("groups").exec(function(err, result){
+	    		if (err) throw err;
+	    		context.memberOf = result.groups;
+	    		Group.find({"owner":req.user.id}).populate("members").exec(function(err, result){
+	    			if (err) throw err;
+	    			context.result = result;
+	    			context.status = "success";
+	    			return res.json(context);
+	    		});
+	    	});
+	    	
+	    } catch (err) {return res.json(context);}
   	},
 
   	include_member: function (req, res) {
@@ -113,7 +147,7 @@
 
 	    console.log(req.body);
 
-	    var data = (req.body.formdata) ? req.body.formdata : undefined;
+	    var data = (req.body) ? req.body : undefined;
 	    if (data) {
 	      	try {
       			data.owner = req.user.id;
